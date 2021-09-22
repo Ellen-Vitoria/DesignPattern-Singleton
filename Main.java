74d321574e8ecc5e5d2bfff5646e87321fb8a102
ellen <ellen.vitoria.ogs@gmail.com>
2021-09-22 17:15:07 -0300

    Demonstrando Singleton

--- Main.java
+++ Main.java
@@ -1,1 +1,6 @@
-
+public class Main {
+    public static void main(String[] args){
+        Fila fl = Fila.getInstance();
+        System.out.println("Demonstração do Design Pattern - Singleton");
+    }
+}
